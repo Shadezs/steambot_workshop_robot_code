91445ed32ac9d460fae1ac04cdc96e4f3a30862a
Shadezs <carlosdakk@yahoo.com>
2019-11-08 19:18:12 -0800

    update autonomus and drive control

--- Iron Bot/1877C_Comp_template.py
+++ Iron Bot/1877C_Comp_template.py
@@ -4,16 +4,16 @@ import vex
 from vex import *
 
 #region config
-brain      = vex.Brain()
-lroller    = vex.Motor(vex.Ports.PORT1, vex.GearSetting.RATIO18_1, False)
-lfront     = vex.Motor(vex.Ports.PORT3, vex.GearSetting.RATIO18_1, False)
-mag        = vex.Motor(vex.Ports.PORT4, vex.GearSetting.RATIO18_1, False)
-lback      = vex.Motor(vex.Ports.PORT5, vex.GearSetting.RATIO18_1, False)
-rback      = vex.Motor(vex.Ports.PORT6, vex.GearSetting.RATIO18_1, False)
-rfront     = vex.Motor(vex.Ports.PORT7, vex.GearSetting.RATIO18_1, False)
-rollerlift = vex.Motor(vex.Ports.PORT9, vex.GearSetting.RATIO18_1, False)
-rroller    = vex.Motor(vex.Ports.PORT10, vex.GearSetting.RATIO18_1, False)
-con        = vex.Controller(vex.ControllerType.PRIMARY)
+brain        = vex.Brain()
+leftmotor    = vex.Motor(vex.Ports.PORT11, vex.GearSetting.RATIO18_1, False)
+left_intake  = vex.Motor(vex.Ports.PORT14, vex.GearSetting.RATIO18_1, False)
+left_lift    = vex.Motor(vex.Ports.PORT15, vex.GearSetting.RATIO18_1, False)
+rightmotor   = vex.Motor(vex.Ports.PORT17, vex.GearSetting.RATIO18_1, True)
+right_lift   = vex.Motor(vex.Ports.PORT18, vex.GearSetting.RATIO18_1, True)
+tray         = vex.Motor(vex.Ports.PORT19, vex.GearSetting.RATIO18_1, False)
+right_intake = vex.Motor(vex.Ports.PORT20, vex.GearSetting.RATIO18_1, True)
+dt           = vex.Drivetrain(leftmotor, rightmotor, 101.6, 28.9, vex.DistanceUnits.MM)
+con          = vex.Controller(vex.ControllerType.PRIMARY)
 #endregion config
 
 
@@ -23,65 +23,54 @@ competition = vex.Competition()
 def pre_auton():
     # All activities that occur before competition start
     # Example: setting initial positions
+    leftmotor.set_stopping(vex.BrakeType.COAST)
+    rightmotor.set_stopping(vex.BrakeType.COAST)
+    dt.set_velocity(100,vex.VelocityUnits.PCT)
     pass
 
 def autonomous():
-   #Blue
-   Forward
-   Right
-   Back
-   Forward
-   Intake and forward
-   Back
-   Left
-   Forward
-   Score
-  #Red
-   Forward
-   Left
-   Back
-   Forward
-   Intake and forward
-   Back
-   Right
-   Forward
-   Score
-   
-   
- # Place autonomous code here
+    # Place autonomous code here
+    tray.rotate_for(vex.DirectionType.REV,5,vex.RotationUnits.REV,50,vex.VelocityUnits.PCT)
+    tray.rotate_for(vex.DirectionType.FWD,5,vex.RotationUnits.REV,50,vex.VelocityUnits.PCT,False)
+    dt.drive_for(vex.DirectionType.FWD,6,vex.DistanceUnits.IN)
+    dt.turn_for(vex.TurnType.LEFT,90,vex.RotationUnits.DEG)
+    dt.drive_for(vex.DirectionType.FWD,6,vex.DistanceUnits.IN)
     pass
 
 def drivercontrol():
     # Place drive control code here, inside the loop
     while True:
-        lfront.spin(vex.DirectionType.FWD,(con.axis3.value()),vex.VelocityUnits.PCT)
-        rfront.spin(vex.DirectionType.FWD, (con.axis2.value()), vex.VelocityUnits.PCT)
-        lback.spin(vex.DirectionType.FWD, (con.axis3.value()), vex.VelocityUnits.PCT)
-        rback.spin(vex.DirectionType.FWD, (con.axis2.value()), vex.VelocityUnits.PCT)
-# 
+        # This is the main loop for the driver control.
+        # Each time through the loop you should update motor
+        # movements based on input from the controller.
+        leftmotor.spin(vex.DirectionType.FWD,(con.axis3.value()),vex.VelocityUnits.PCT)
+        rightmotor.spin(vex.DirectionType.FWD,(con.axis2.value()),vex.VelocityUnits.PCT)
         if con.buttonR1.pressing():
-            rroller.spin(vex.DirectionType.FWD, 100, vex.VelocityUnits.PCT)
-            lroller.spin(vex.DirectionType.FWD, 100, vex.VelocityUnits.PCT)
+            right_intake.spin(vex.DirectionType.FWD,100,vex.VelocityUnits.PCT)
+            left_intake.spin(vex.DirectionType.FWD,100,vex.VelocityUnits.PCT)
         elif con.buttonR2.pressing():
-            rroller.spin(vex.DirectionType.FWD, -100, vex.VelocityUnits.PCT)
-            lroller.spin(vex.DirectionType.FWD, -100, vex.VelocityUnits.PCT)
-        else:
-            rroller.stop(vex.BrakeType.BRAKE)
-            lroller.stop(vex.BrakeType.BRAKE)
+            right_intake.spin(vex.DirectionType.FWD,-100,vex.VelocityUnits.PCT)
+            left_intake.spin(vex.DirectionType.FWD,-100,vex.VelocityUnits.PCT)
+        else :
+            right_intake.stop(vex.BrakeType.BRAKE)
+            left_intake.stop(vex.BrakeType.BRAKE)
         if con.buttonL1.pressing():
-            rollerlift.spin(vex.DirectionType.FWD, 100, vex.VelocityUnits.PCT)
+            left_lift.spin(vex.DirectionType.FWD,100,vex.VelocityUnits.PCT)
+            right_lift.spin(vex.DirectionType.FWD,100,vex.VelocityUnits.PCT)
         elif con.buttonL2.pressing():
-            rollerlift.spin(vex.DirectionType.FWD, -100, vex.VelocityUnits.PCT)
+            left_lift.spin(vex.DirectionType.FWD,-100,vex.VelocityUnits.PCT)
+            right_lift.spin(vex.DirectionType.FWD,-100,vex.VelocityUnits.PCT)
         else:
-            rollerlift.stop(vex.BrakeType.HOLD)
-        if con.buttonX.pressing():
-            mag.spin(vex.DirectionType.FWD, 100, vex.VelocityUnits.PCT)
+            left_lift.stop(vex.BrakeType.BRAKE)
+            right_lift.stop(vex.BrakeType.BRAKE)
+        if con.buttonA.pressing():
+            tray.spin(vex.DirectionType.FWD,100,vex.VelocityUnits.PCT)
         elif con.buttonB.pressing():
-            mag.spin(vex.DirectionType.FWD, -100, vex.VelocityUnits.PCT)
-        else:
-            mag.stop(vex.BrakeType.BRAKE)
-
+            tray.spin(vex.DirectionType.FWD,-100,vex.VelocityUnits.PCT)
+        else :
+            tray.stop(vex.BrakeType.BRAKE)    
         pass
+        
 
 # Do not adjust the lines below
 
